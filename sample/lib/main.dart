b19ee81a146047939992cac425b9d0d5755232b4
lakshaycodesgit <lakshay.gulati.ug20@nsut.ac.in>
2022-04-10 12:23:46 +0530

    our app

---
+++ sample/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:sample/pages/home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          brightness: Brightness.dark,
+          primaryColor: Colors.deepPurple,
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.deepPurple)),
+          ),
+          appBarTheme: AppBarTheme(elevation: 0)),
+    );
+  }
+}
---
+++ sample/lib/utils/constants.dart
@@ -0,0 +1,10 @@
+// ignore: non_constant_identifier_names
+String infura_url =
+    "https://kovan.infura.io/v3/7dd1e6e298ff43e0a27240d687e8f4f5";
+// ignore: non_constant_identifier_names
+String owner_private_key =
+    "1bc57d1c69ed7178ef78ae18935f5d399ca662a04730c32f8e15c51e60496477";
+// ignore: non_constant_identifier_names
+String voter_private_key =
+    "779084d68c7763690a79a6fa1fce36f08c610343b2d585f4e9e1367d18ce0097";
+String contractAddress1 = "0x8C0C8368B2397B41ED5188b6d7D2A136b9f113c7";
