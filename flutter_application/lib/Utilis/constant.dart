961ca307f808e046e65cbcc2662b4a4887f53836
yash sharma <76558099+yash558@users.noreply.github.com>
2022-04-10 07:17:55 +0530

    first commit

---
+++ flutter_application/lib/Utilis/constant.dart
@@ -0,0 +1,8 @@
+String infura_url =
+    "https://27Zk2hNjAi2bQPjPqUWrCUqlyJB:60aefdedc3e834dd6388b7c803e8859f@eth2-beacon-mainnet.infura.io";
+
+String owner_private_key =
+    "33a87d75e27615992b64e4e6772d1043a1cbd68ca36fe0d41175c0a51ffa9f44";
+
+String voter_private_key =
+    "4636a1a66afddbb6690d73c4b22157fa36ca8600aae31dcbfdb23ad87db5106c";
---
+++ flutter_application/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.deepPurple,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Colors.deepOrange),
+          ),
+            appBarTheme: AppBarTheme(elevation: 0),
+          ),
+      ),
+    );
+  }
+}
